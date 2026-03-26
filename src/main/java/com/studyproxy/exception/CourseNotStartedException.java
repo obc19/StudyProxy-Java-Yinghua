0e0dec35d85220445a8304f8dffc115f97fb98c4
obc19 <xmy213116@163.com>
2026-03-26 13:13:20 +0800

    第一次版本提交，以完成英华学堂课程视频功能。

--- src/main/java/com/studyproxy/exception/CourseNotStartedException.java
+++
@@ -1,7 +0,0 @@
-package com.studyproxy.exception;
-
-public class CourseNotStartedException extends RuntimeException {
-    public CourseNotStartedException(String message) {
-        super(message);
-    }
-}
